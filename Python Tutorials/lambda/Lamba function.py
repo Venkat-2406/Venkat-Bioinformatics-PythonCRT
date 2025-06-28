d29c949add8bdb8378916dc0e5258832cfaa85d1
Venkat-2406 <reddykv431@gmail.com>
2025-06-28 16:43:50 +0530

    Add files via upload

---
+++ Python Tutorials/lambda/Lamba function.py
@@ -0,0 +1,4 @@
+#   SYNTAX........  lamba argument_list : expression
+res=lambda x,y:x+y
+print(res(2,3))
+
---
+++ Python Tutorials/lambda/positive or negative(lambda).py
@@ -0,0 +1,3 @@
+#Program to check the pos or neg 
+num=lambda x : print("+ve") if(x>0) else print("-Ve") if(x<0) else print("xero")
+num(0)
